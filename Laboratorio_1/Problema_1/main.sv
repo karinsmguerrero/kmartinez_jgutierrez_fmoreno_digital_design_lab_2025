c1325150566e529fe71d2d6a22743da4bf79e249
Karina Martinez <karinamguerrero@estudiantec.cr>
2025-03-11 14:14:46 -0600

    Fixed pin assignment and register index

--- Laboratorio_1/Problema_1/main.sv
+++ Laboratorio_1/Problema_1/main.sv
@@ -1,3 +1,5 @@
+`default_nettype none   //Para que verilog no haga cables raros
+
 module main( 
 
 		//input   logic [ 3: 0]   bin,  
@@ -6,7 +8,7 @@ module main(
       output  logic [ 6: 0]   HEX1,
 		
 		///////// SW /////////
-      input    [ 9: 0]   SW,
+      input    [ 9: 0]   SW
 		
 		///////// KEY /////////
       //input    [ 3: 0]   KEY
@@ -16,7 +18,7 @@ module main(
 	
 	//BinToBCD converter(bin, bcd); 
 	BinToBCD converter(SW[3:0], bcd);
-	seven_segment_driver seg0(bcd[3:0], HEX0);
-	seven_segment_driver seg1(bcd[7:4], HEX1);
+	seven_segment_driver seg0(bcd[11:8], HEX1);
+	seven_segment_driver seg1(bcd[7:4], HEX0);
 	 
 endmodule
