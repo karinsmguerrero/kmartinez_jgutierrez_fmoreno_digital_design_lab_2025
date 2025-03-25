2eacfe5c3be342c43ed94ee0039dfca56426414f
Karina Martinez <karinamguerrero@estudiantec.cr>
2025-03-24 20:00:06 -0600

    added overflow flag

--- Laboratorio_2/Problema 1/multiplier.sv
+++ Laboratorio_2/Problema 1/multiplier.sv
@@ -4,17 +4,18 @@ module multiplier
 	input  logic clk,rst,
 	input  logic [N-1:0] A, 
 	input  logic [N-1:0] B, 
-	output logic [N*2 +1 : 0] result
+	output logic [N*2 -1: 0] result,
+	output logic overflow
 );
 
-integer i, length;
-assign last = N*2 -1;
+integer i;
 
 always@(posedge clk or posedge rst)
 	begin
 		if (rst)
 			begin
 				result=0;
+				overflow=0;
 			end
 		else 
 			begin
@@ -24,11 +25,15 @@ always@(posedge clk or posedge rst)
 					begin
 						if(result[0]==1'b1)
 							begin
-								result[N*2: N]=result[N*2:N] + A;
+								result[N*2 -1: N]=result[N*2 -1:N] + A;
 								
 							end
 						result = result >> 1;
 					end
+				if(result[N*2 -1:N] === 0)
+					overflow = 0;
+				else
+					overflow = 1;
 			end
 	end
 endmodule
--- Laboratorio_2/Problema 1/multiplier_tb.sv
+++ Laboratorio_2/Problema 1/multiplier_tb.sv
@@ -1,10 +1,10 @@
 module multiplier_tb;
-	logic clk, rst;
+	logic clk, rst, overflow;
 	logic [3:0] A;
 	logic [3:0] B;
-	wire [8:0] result;
+	logic [7:0] result;
 
-	multiplier dut(clk, rst, A, B, result);
+	multiplier dut(clk, rst, A, B, result, overflow);
 
 
 	// Generador de reloj
@@ -19,7 +19,7 @@ module multiplier_tb;
 		A = 4'b0101;
 		B = 4'b0010;
 		#50;
-		assert (result[8:0] == 9'b000001010) $display ("Passed");
+		assert (result[7:0] == 8'b00001010) $display ("Passed");
 		else $error("Failed: 1");
 		rst = 1;
 		#5;
@@ -28,7 +28,7 @@ module multiplier_tb;
 		A = 4'b1011;
 		B = 4'b0010;
 		#50;
-		assert (result[8:0] == 9'b000010110) $display ("Passed");
+		assert (result[7:0] == 8'b00010110) $display ("Passed");
 		else $error("Failed: 2");
 		rst = 1;
 		#5;
@@ -37,7 +37,18 @@ module multiplier_tb;
 		A = 4'b1001;
 		B = 4'b0101;
 		#50;
-		assert (result[8:0] == 9'b000101101) $display ("Passed");
+		assert (result[7:0] == 8'b00101101) $display ("Passed");
 		else $error("Failed: 3");
+		rst = 1;
+		#5;
+		
+		rst = 0;
+		A = 4'b0011;
+		B = 4'b0010;
+		#50;
+		assert (result[7:0] == 8'b00000110) $display ("Passed");
+		else $error("Failed: 3");
+		rst = 1;
+		//#5;
 	end
 endmodule
