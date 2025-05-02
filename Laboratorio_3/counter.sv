45dc594e40722e32631b9320c52af2e52f038621
Karina Martinez <karinamguerrero@estudiantec.cr>
2025-05-01 18:26:41 -0600

    [add] image files

--- Laboratorio_3/counter.sv
+++ Laboratorio_3/counter.sv
@@ -1,10 +1,26 @@
-module counter #(parameter N = 8)(
-	input logic rst, clk, enable,
-	output logic [N-1:0] Q
+module counter #(parameter N) (
+	input clk,
+	input enable,
+	input reset, 
+	input [N-1:0] max,
+	output done,
+	output reg [N-1:0] q
 );
 
-always_ff @(negedge clk or posedge rst)
-	if(rst) Q = 8'b0;
-	else if(enable) Q = Q + 1'b1;
+assign done = (q == max);
+
+always @(posedge clk) begin
+	if(reset)
+		q <= 0;
+	else begin
+		if(enable) begin
+			if (q==max) begin
+				q <= 0;
+			end else begin
+				q <= q+1;
+			end
+		end
+	end
+end
 
 endmodule
--- Laboratorio_3/vga_driver.sv
+++ Laboratorio_3/vga_driver.sv
@@ -10,15 +10,19 @@ module vga_driver(
 	output logic [7:0] VGA_B,
 	output logic [7:0] VGA_G, 
 	output logic VGA_CLK, 
-	output logic VGA_BLANK_N, VGA_SYNC_N
+	output logic VGA_BLANK_N
 );
 
-	logic [9:0] counter_x = 0;  // horizontal counter
-	logic [9:0] counter_y = 0;  // vertical counter
+	logic [9:0] x = 0;  // horizontal counter
+	logic [9:0] y = 0;  // vertical counter
+	logic [9:0] x_max = 799;
+	logic [9:0] y_max = 524;
+	logic done;
+	
 	logic [7:0] r_red = 0;
 	logic [7:0] r_blue = 0;
 	logic [7:0] r_green = 0;
-	
+
 	
 	logic clk25MHz;
 
@@ -29,220 +33,203 @@ module vga_driver(
 		.outclk_0(clk25MHz),
 		.locked()
 		); 
-
+	assign VGA_CLK = clk25MHz;
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// counter and sync generation
-	assign VGA_CLK = clk25MHz;
-	
+	// counter and sync generation	
 	video_sync_generator video_sync (
 		.vga_clk(VGA_CLK),
       .reset(reset),
 		.blank_n(VGA_BLANK_N),
 		.HS(VGA_HS),
 		.VS(VGA_VS));
-			
-	always @(posedge clk25MHz)  // horizontal counter
-		begin 
-			if (counter_x < 799)
-				counter_x <= counter_x + 1;  // horizontal counter (including off-screen horizontal 160 pixels) total of 800 pixels 
-			else
-				counter_x <= 0;              
-		end  // always 
 		
-	always @ (posedge clk25MHz)  // vertical counter
-		begin 
-			if (counter_x == 799)  // only counts up 1 count after horizontal finishes 800 counts
-				begin
-					if (counter_y < 524)  // vertical counter (including off-screen vertical 45 pixels) total of 525 pixels
-						counter_y <= counter_y + 1;
-				else
-						counter_y <= 0;              
-				end  // if (counter_x...
-	end  // always
-	// end counter and sync generation  
 
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// hsync and vsync output assignments
+	counter #(10) counter_x(.clk(clk25MHz), .enable(1), .reset(reset), .max(x_max), .done(done), .q(x));
+	counter #(10) counter_y(.clk(clk25MHz), .enable(done), .reset(reset), .max(y_max), .done(), .q(y));
 	
-	// end hsync and vsync output assignments
+	parameter imgSize = 24300;   // image data : 90 * 90 *3 bytes
+	reg [7 : 0]   total_memory [0 : imgSize-1];// memory to store  8-bit data image
+	initial begin
+		$readmemh("./img/tile.hex",total_memory,0,imgSize-1);
+	end			
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// pattern generate
 		always @ (posedge clk25MHz)
 		begin
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 1
-			if (counter_y < 135)
+			if (y < 135)
 				begin              
 					r_red <= 8'hFF;    // white
 					r_blue <= 8'hFF;
 					r_green <= 8'hFF;
-				end  // if (counter_y < 135)
+				end  // if (y < 135)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 1
 			
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 2
-			else if (counter_y >= 135 && counter_y < 205)
+			else if (y >= 135 && y < 205)
 				begin 
-					if (counter_x < 324)
+					if (x < 324)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // if (counter_x < 324)
-					else if (counter_x >= 324 && counter_x < 604)
+						end  // if (x < 324)
+					else if (x >= 324 && x < 604)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 324 && counter_x < 604)
-					else if (counter_x >= 604)
+						end  // else if (x >= 324 && x < 604)
+					else if (x >= 604)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 604)
-					end  // else if (counter_y >= 135 && counter_y < 205)
+						end  // else if (x >= 604)
+					end  // else if (y >= 135 && y < 205)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 2
 			
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 3
-			else if (counter_y >= 205 && counter_y < 217)
+			else if (y >= 205 && y < 217)
 				begin 
-					if (counter_x < 324)
+					if (x < 324)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // if (counter_x < 324)
-					else if (counter_x >= 324 && counter_x < 371)
+						end  // if (x < 324)
+					else if (x >= 324 && x < 371)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 324 && counter_x < 371)
-					else if (counter_x >= 371 && counter_x < 383)
+						end  // else if (x >= 324 && x < 371)
+					else if (x >= 371 && x < 383)
 						begin 
 							r_red <= 8'h00;    // black
 							r_blue <= 8'h00;
 							r_green <= 8'h00;
-						end  // else if (counter_x >= 371 && counter_x < 383)
-					else if (counter_x >= 383 && counter_x < 545)
+						end  // else if (x >= 371 && x < 383)
+					else if (x >= 383 && x < 545)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 383 && counter_x < 545)
-					else if (counter_x >= 545 && counter_x < 557)
+						end  // else if (x >= 383 && x < 545)
+					else if (x >= 545 && x < 557)
 						begin 
 							r_red <= 8'h00;    // black
 							r_blue <= 8'h00;
 							r_green <= 8'h00;
-						end  // else if (counter_x >= 545 && counter_x < 557)
-					else if (counter_x >= 557 && counter_x < 604)
+						end  // else if (x >= 545 && x < 557)
+					else if (x >= 557 && x < 604)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 557 && counter_x < 604)
-					else if (counter_x >= 604)
+						end  // else if (x >= 557 && x < 604)
+					else if (x >= 604)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 604)
-				end  // else if (counter_y >= 205 && counter_y < 217)
+						end  // else if (x >= 604)
+				end  // else if (y >= 205 && y < 217)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 3
 			
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 4
-			else if (counter_y >= 217 && counter_y < 305)
+			else if (y >= 217 && y < 305)
 				begin
-					if (counter_x < 324)
+					if (x < 324)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // if (counter_x < 324)
-					else if (counter_x >= 324 && counter_x < 604)
+						end  // if (x < 324)
+					else if (x >= 324 && x < 604)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 324 && counter_x < 604)
-					else if (counter_x >= 604)
+						end  // else if (x >= 324 && x < 604)
+					else if (x >= 604)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 604)	
-				end  // else if (counter_y >= 217 && counter_y < 305)
+						end  // else if (x >= 604)	
+				end  // else if (y >= 217 && y < 305)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 4
 			
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 5
-			else if (counter_y >= 305 && counter_y < 310)
+			else if (y >= 305 && y < 310)
 				begin
-					if (counter_x < 324)
+					if (x < 324)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // if (counter_x < 324)
-					else if (counter_x >= 324 && counter_x < 371)
+						end  // if (x < 324)
+					else if (x >= 324 && x < 371)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 324 && counter_x < 371)
-					else if (counter_x >= 371 && counter_x < 557)
+						end  // else if (x >= 324 && x < 371)
+					else if (x >= 371 && x < 557)
 						begin 
 							r_red <= 8'h00;    // black
 							r_blue <= 8'h00;
 							r_green <= 8'h00;
-						end  // else if (counter_x >= 371 && counter_x < 557)
-					else if (counter_x >= 557 && counter_x < 604)
+						end  // else if (x >= 371 && x < 557)
+					else if (x >= 557 && x < 604)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 557 && counter_x < 604)
-					else if (counter_x >= 604)
+						end  // else if (x >= 557 && x < 604)
+					else if (x >= 604)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 604)	
-				end  // else if (counter_y >= 217 && counter_y < 305)
+						end  // else if (x >= 604)	
+				end  // else if (y >= 217 && y < 305)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 5
 			
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 6
-			else if (counter_y >= 305 && counter_y < 414)
+			else if (y >= 305 && y < 414)
 				begin
-					if (counter_x < 324)
+					if (x < 324)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // if (counter_x < 324)
-					else if (counter_x >= 324 && counter_x < 604)
+						end  // if (x < 324)
+					else if (x >= 324 && x < 604)
 						begin 
 							r_red <= 8'hFF;    // yellow
 							r_blue <= 8'h00;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 324 && counter_x < 604)
-					else if (counter_x >= 604)
+						end  // else if (x >= 324 && x < 604)
+					else if (x >= 604)
 						begin 
 							r_red <= 8'hFF;    // white
 							r_blue <= 8'hFF;
 							r_green <= 8'hFF;
-						end  // else if (counter_x >= 604)	
-				end  // else if (counter_y >= 305 && counter_y < 414)
+						end  // else if (x >= 604)	
+				end  // else if (y >= 305 && y < 414)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 6
 			
 			////////////////////////////////////////////////////////////////////////////////////// SECTION 7
-			else if (counter_y <= 414)
+			else if (y <= 414)
 				begin              
 					r_red <= 8'hFF;    // white
 					r_blue <= 8'hFF;
 					r_green <= 8'hFF;
-				end  // if (counter_y >= 414)
+				end  // if (y >= 414)
 			////////////////////////////////////////////////////////////////////////////////////// END SECTION 7
 		end  // always
 						
@@ -251,9 +238,9 @@ module vga_driver(
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// color output assignments
 	// only output the colors if the counters are within the adressable video time constraints
-	assign VGA_R = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_red : 8'hFF;
-	assign VGA_B = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_blue : 8'hFF;
-	assign VGA_G = (counter_x > 144 && counter_x <= 783 && counter_y > 35 && counter_y <= 514) ? r_green : 8'hFF;
+	assign VGA_R = (x > 144 && x <= 783 && y > 35 && y <= 514) ? r_red : 8'hFF;
+	assign VGA_B = (x > 144 && x <= 783 && y > 35 && y <= 514) ? r_blue : 8'hFF;
+	assign VGA_G = (x > 144 && x <= 783 && y > 35 && y <= 514) ? r_green : 8'hFF;
 	// end color output assignments
 	
 endmodule  // VGA_image_gen
